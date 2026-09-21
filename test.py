2a715a619d36448dad5ad62e4f43d7c5a4309f2b
SilvesterSpath <spath.sz@gmail.com>
2026-09-21 07:44:55 +0200

    test: test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate.from_messages([
+  ("human", "{content}")
+])
+
+messages = prompt.format_messages(content="Tell me a joke")
+
+output = chat(messages)
+
+print(output.content)
